422ae38de4ec049196e28af94962934d70807b22
Zoltán Szőgyényi <szogyenyi.zoltan.lorand@gmail.com>
2025-03-19 16:51:15 +0200

    initial commit

---
+++ flowbiteapp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def accordion(request):
+    return render(request, 'accordion.html')
+
+def carousel(request):
+    return render(request, 'carousel.html')
+
+def collapse(request):
+    return render(request, 'collapse.html')
+
+def dial(request):
+    return render(request, 'dial.html')
+
+def dismiss(request):
+    return render(request, 'dismiss.html')
+
+def modal(request):
+    return render(request, 'modal.html')
+
+def drawer(request):
+    return render(request, 'drawer.html')
+
+def dropdown(request):
+    return render(request, 'dropdown.html')
+
+def popover(request):
+    return render(request, 'popover.html')
+
+def tooltip(request):
+    return render(request, 'tooltip.html')
+
+def tabs(request):
+    return render(request, 'tabs.html')
+
+def input_counter(request):
+    return render(request, 'input-counter.html')
+
+def datepicker(request):
+    return render(request, 'datepicker.html')
